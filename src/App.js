7520bc5d302404a2ddd5e8841880437c4cc5472b
Marco Arredondo <marcoarredondoz28@gmail.com>
2023-01-02 11:43:03 -0600

    Installing react-router-dom and creating basic routing structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {HashRouter, Routes, Route} from "react-router-dom";
+import { Menu } from "./Menu";
+import { BlogPage } from "./BlogPage";
+import { HomePage } from "./HomePage";
+import { ProfilePage } from "./ProfilePage";
+
+//Creating hash router #
+// /#/blog
+// /#/profile
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blob" element={<BlogPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="*" element={<p>Not Found</p>} />
+      </Routes>
+    </HashRouter>
   );
 }
 
