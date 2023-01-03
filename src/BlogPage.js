4f04a236b837065457e931d9ad7d27b23661280b
Marco Arredondo <marcoarredondoz28@gmail.com>
2023-01-03 12:48:17 -0600

    Adding Outlet coponent to use nested routes

--- src/BlogPage.js
+++ src/BlogPage.js
@@ -1,16 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { blogData } from "./blogdata";
 
 function BlogPage(){
     return (
        <>
             <h1>Blog Page</h1>
+
             <ul>
                 {blogData.map(post => (
                     <BlogLink key={post.slug} post={post}/>
                 ))}
             </ul>
+
+            <Outlet></Outlet>
            
        </>
     );
